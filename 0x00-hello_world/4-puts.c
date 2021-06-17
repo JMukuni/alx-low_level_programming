6d2c6ba5a25fc9d8274708497e74ac1e50460a21
JMukuni <bossjefter93@gmail.com>
2021-06-17 12:21:49 -0700

    Puts again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * Description: Prints "\"Programming is like building a multilingual puzzle"
  * Return: 0
  */
-int main(main)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle\n");
 getchar();
