f024a8e0b85348884f4c3e46c79717d0b54eb1f5
JMukuni <bossjefter93@gmail.com>
2021-06-20 20:51:55 -0700

    Determine tatus of random number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,22 +12,10 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-{
-if (n == 0)
-{
-  printf("The number is zero\n");
-}
-else
-{
 printf("The numberis positive\n");
-}
-}
+else if (n == 0)
+printf("The number is zero\n");
 else
-{
 printf("The numberis negative\n");
-}
-}
-int n;
-printf("%d\n", n);
 return (0);
 }
