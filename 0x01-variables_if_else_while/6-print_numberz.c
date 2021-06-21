67fd6481a443d7304be5e4fafdd1bb425dac49e2
JMukuni <bossjefter93@gmail.com>
2021-06-21 12:43:47 -0700

    Print numbers without char variables

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- print all numbers under 10 without using char variables
+ * Return: 0
+ */
+int main(void)
+{
+int num = 48;
+while (num < 58)
+{
+putchar(num);
+num++;
+}
+putchar('\n');
+return (0);
+}
