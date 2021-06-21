98bc9f707569228e41a691849da5a9050184e2a9
JMukuni <bossjefter93@gmail.com>
2021-06-20 21:02:45 -0700

    Determine status of a number generated

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,8 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /*
- *main: void function
- *Description: writing control statements
+ *main -  writing control statements
  *return 0
 */
 int main(void)
@@ -12,10 +11,10 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-printf("The numberis positive\n");
+printf("%d is positive\n", n);
 else if (n == 0)
-printf("The number is zero\n");
+printf("%d is zero\n", n);
 else
-printf("The numberis negative\n");
+printf("%d is negative\n", n);
 return (0);
 }
