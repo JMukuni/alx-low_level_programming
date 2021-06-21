3cf20ed08cd982087ce517e70003ac8002d3f4ec
JMukuni <bossjefter93@gmail.com>
2021-06-21 13:59:50 -0700

    Create pairs of numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main- pairs of numbers that do not repeat
+ * Return: 0
+ */
+int main(void)
+{
+int j, i;
+for (j = 0; j <= 99; j++)
+{
+for (i = j; i <= 99; i++)
+{
+f (i != j)
+{
+putchar(j / 10 + 48);
+putchar(j % 10 + 48);
+putchar(' ');
+utchar(i / 10 + 48);
+putchar(i % 10 + 48);
+if (j * 100 + i != 9899)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
