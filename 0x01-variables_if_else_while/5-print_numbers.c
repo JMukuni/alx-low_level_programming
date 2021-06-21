b6ef4b7e2f86817afbf87f3879c4b66e86772a33
JMukuni <bossjefter93@gmail.com>
2021-06-21 12:30:19 -0700

    Print all single decimals

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main- print all single decimal numbers including 0
+ * Return: 0
+ */
+int main(void)
+{
+int num;
+for (num = 0; num < 10; i++)
+printf("%d", num);
+printf("\n");
+return (0);
+}
