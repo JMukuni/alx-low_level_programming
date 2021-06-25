0ef2ab44f455ce4cefeb996e5f288ebd073dc1e4
JMukuni <bossjefter93@gmail.com>
2021-06-24 22:25:24 -0700

    Return different results in different cases concerning same character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * _islower - return 1 if c is lowercase or 0 otherwise
+ * @c: integer to be tested
+ * Return: 1 or 0
+ */
+int _islower(int c)
+{
+return (c >= 97 && c <= 122);
+}
