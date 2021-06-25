c05622c619f606ef7710bdd929f87e130329631f
JMukuni <bossjefter93@gmail.com>
2021-06-24 22:10:31 -0700

    Print alphabet in lower cases

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * print_alphabet - print alphabet in lower cases using _putchar
+ */
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+_putchar(c);
+_putchar('\n');
+}
