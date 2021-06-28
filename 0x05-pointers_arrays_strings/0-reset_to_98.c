7387861853dada7375d76b8c725fd582f2fc7ac2
JMukuni <bossjefter93@gmail.com>
2021-06-28 06:46:29 -0700

    Take pointer for an int

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,9 @@
 #include "holberton.h"
 /**
  *reset_to_98 - takes a pointer to an int
- *@n - variable n
- *@*n - pointer to variable n
+ *@n - pointer n
  */
 void reset_to_98(int *n)
 {
-int n = 98;
-int *n = &n;
-_putchar('%p', &n);
-_putchar('\n')
+*n = 98
 }
