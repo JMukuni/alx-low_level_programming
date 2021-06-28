62b340c4c117359b27533000226cc825fbdda6a1
JMukuni <bossjefter93@gmail.com>
2021-06-28 12:14:06 -0700

    Create valid password for a program

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,8 +8,18 @@
  */
 int main(void)
 {
-srand(time(0));
-int num = rand();
-rintf("%i\n", num);
-return (0);
+  int r = 0, c = 0;
+  time_t t;
+
+  srand((unsigned int) time(&t));
+  while (c < 2772)
+    {
+      r = rand() % 128;
+      if ((c + r) > 2772)
+	break;
+      c = c + r;
+      printf("%c", r);
+    }
+  printf("%c\n", (2772 - c));
+  return (0);
 }
