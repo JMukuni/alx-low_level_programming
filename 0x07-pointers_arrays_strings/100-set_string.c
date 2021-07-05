95eecfa6bd52a8b0d26f7f1f82c41848ee9667b9
JMukuni <bossjefter93@gmail.com>
2021-07-05 02:40:53 -0700

    Set vslue of a pointer to char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * set_string - set value of a pointer to a char.
+ * @s: double pointer to be copied
+ * @to: where s gets set to
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
