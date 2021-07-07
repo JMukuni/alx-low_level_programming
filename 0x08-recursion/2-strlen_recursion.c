e1bdf54bd61d12f42a1d6694e90df68f8eb60a9f
JMukuni <bossjefter93@gmail.com>
2021-07-06 23:51:36 -0700

    Return string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * _strlen_recursion - return length of string
+ * @s: input string
+ * Return: length of s
+ */
+int _strlen_recursion(char *s)
+{
+if (!*s)
+return (0);
+return (1 + _strlen_recursion(++s));
+}
