e26adb462d6a3eeac248b7c156a00548158d1f0d
JMukuni <bossjefter93@gmail.com>
2021-07-13 10:34:16 -0700

    Free 2 dimensional array previously created

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * free_grid - free 2 dimensional grid created by the alloc_grid function
+ * @grid: double pointer to multi-dimen array
+ * @height: height of grid
+ */
+void free_grid(int **grid, int height)
+{
+int i;
+for (i = 0; i < height; i++)
+free(grid[i]);
+free(grid);
+}
