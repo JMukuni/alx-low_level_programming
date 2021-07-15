0465caa5374301b69b7280d91ae184c9098cc221
JMukuni <bossjefter93@gmail.com>
2021-07-15 04:36:24 -0700

    Allocate memory using calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ * _calloc - allocate memory for array using malloc
+ * @nmemb: number of elements of pointer
+ * @size: size of each member
+ * Return: pointer of allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+void *ptr;
+if (!nmemb || !size)
+return (NULL);
+ptr = malloc(size * nmemb);
+if (!ptr)
+return (NULL);
+_memset(ptr, 0, size * nmemb);
+return (ptr);
+}
+/**
+ * _memset - fill memory with constant byte
+ * @s: memory area
+ * @b: constant byte
+ * @n: bytes of the memory area
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+char *ptr = s;
+while (n--)
+*s++ = b;
+return (ptr);
+}
