305be08a15f047bbf0b41849172b39b7b876bed0
JMukuni <bossjefter93@gmail.com>
2021-07-19 02:53:57 -0700

    Add dog.h file and headers

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
 #include <stdlib.h>
-#include "dog.h"
 /**
- * init_dog - initialize variable of type struct dog
- * @d: pointer to struct dog to initialize
- * @name: name to initialize
- * @age: age to initialize
- * @owner: owner to initialize
+ * struct dog - dog
+ * @name: name of the dog
+ * @age: how old the dog is
+ * @owner: who owns the dog
  */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+typedef struct dog
 {
-if (d == NULL)
-d = malloc(sizeof(struct dog));
-d->name = name;
-d->age = age;
-d->owner = owner;
-}
+char *name;
+float age;
+char *owner;
+} dog_t;
+int _strlen(char *s);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strdup(char *str);
+#endif
