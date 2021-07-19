2e5efd0c0f319b7dce4bd405a7342226415812d6
JMukuni <bossjefter93@gmail.com>
2021-07-19 00:09:35 -0700

    Print file name from which the program come

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print name of the file the program have compiled from
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
