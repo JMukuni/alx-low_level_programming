da70c10780281180e4968693cfd8b73afbae1f60
JMukuni <bossjefter93@gmail.com>
2021-08-05 02:36:55 -0700

    Determine size of endian

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * get_endianness - check if a machine is little or big endian
+ * Return: 0 for big, 1 for little
+ */
+int get_endianness(void)
+{
+unsigned int i = 1;
+char *c = (char *) &i;
+return (*c);
+}
