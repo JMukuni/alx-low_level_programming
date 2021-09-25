68f766c4a23d747b9fe6118a723fc20853b1fbd3
JMukuni <bossjefter93@gmail.com>
2021-09-25 07:22:12 -0700

    Add files *.c

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * _isupper - checks if input is an uppercase letter in ASCII
+ * Return: 1 if true, 0 if false
+ */
+int _isupper(int c)
+{
+return (c >= 65 && c <= 90);
+}
---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _abs - return absolute value of n
+ * @n: integer to be tested
+ * Return: abs(n)
+ */
+int _abs(int n)
+{
+return (n < 0 ? -1 * n : n);
+}
