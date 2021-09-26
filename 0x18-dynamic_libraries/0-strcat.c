b91169eaa50ccbcca565cc31929331bcb63cf973
JMukuni <bossjefter93@gmail.com>
2021-09-25 23:10:54 -0700

    Remove strlen func

--- 0x18-dynamic_libraries/0-strcat.c
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -1,21 +1,6 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *_strlen - return length of string
- *@str:a string of length to be returned
- *Return: returns the length of a string
- */
-int _strlen(char *str)
-{
-int length = 0;
-while (*str)
-{
-str++;
-length++;
-}
-return (length);
-}
-/**
  *_strcat - concatinates two strings
  *@dest:destination pointer
  *@src:pointer to a string
